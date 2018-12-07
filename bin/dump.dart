fcab29caabc9e0068c53984bedfd0ebfec90fd9e
Edoardo Putti <edoardo.putti@gmail.com>
2018-12-07 18:24:47 +0100

    handle specifying url from command line

--- bin/dump.dart
+++ bin/dump.dart
@@ -1,8 +1,13 @@
 import 'package:http/http.dart';
+import 'package:args/args.dart';
 import '../lib/har.dart';
 
-main() {
-  var client = new Client();
-  var har = new HarClient(client);
-  har.get('https://google.com').then((_) => print(har.toHar()));
+main(List<String> args) {
+  var parser = ArgParser();
+  var result = parser.parse(args);
+  final url = result.rest.first;
+
+  var client = Client();
+  var har = HarClient(client);
+  har.get(url).then((_) => print(har.toHar()));
 }
